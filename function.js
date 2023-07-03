5daee25a2e8b9ce1346dbc84d43ac33fb5209dd0
Silvana A <alfonsosilvana16@gmail.com>
2023-07-03 17:44:25 -0300

    validacion-formulario

--- function.js
+++ function.js
@@ -39,3 +39,55 @@ function efectoHabilidades(){
 window.onscroll = function(){
     efectoHabilidades();
 }
+const form = document.getElementById('form-contact');
+const nombre = document.getElementById('nombre');
+const email = document.getElementById('email');
+const asunto = document.getElementById('asunto');
+const mensaje = document.getElementById('mensaje');
+const error = document.getElementById('error');
+
+form.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  console.log("enviando form");
+  var msjError = [];
+
+  function mostrarError(input, mensaje) {
+    const errorHtml = `<div class="error-message">${mensaje}</div>`;
+    input.insertAdjacentHTML('afterend', errorHtml);
+    input.classList.add('error-input');
+   }
+   
+  function limpiarErrores() {
+    const errorDivs = document.querySelectorAll('.error-message');
+    errorDivs.forEach((div) => div.remove());
+    const inputs = document.querySelectorAll('[data-error]');
+    inputs.forEach((input) => input.classList.remove('error-input'));
+  }
+  limpiarErrores();
+
+
+  if (nombre.value === null || nombre.value === '') {
+    mostrarError(nombre, 'Ingresa tu nombre (hasta 50 caracteres)');
+  }
+  if (email.value === null || email.value === '') {
+    mostrarError(email, 'Ingresa tu email.');
+  } else if (!validarEmail(email.value.trim())) {
+    mostrarError(email, 'El correo electrónico ingresado no es válido.');
+  }
+  if (asunto.value === '') {
+    mostrarError(asunto, 'Este campo es obligatorio (hasta 50 caracteres)');
+  }
+  if (mensaje.value === '') {
+    mostrarError(mensaje, 'Este campo es obligatorio (hasta 300 caracteres)');
+  }
+
+  if (msjError.length === 0) {
+    form.reset(); 
+  }
+
+});
+
+function validarEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
